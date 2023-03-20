cea534484ef74f401f834baa3f42a9d316607eb6
timothykanski <timothykanski@gmail.com>
2023-03-19 23:17:24 -0500

    Connected it all together!!!

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using AuthentiWrite.Service.Models;
 
 var list = new List<Text>();
 
-List<string> contents = FileHelper.GetDocsFromFile("example_H.txt", "|||");
+List<string> contents = FileHelper.GetDocsFromFile("example_AI_3.txt", "|||");
 
 foreach (var txt in contents)
 {
@@ -30,19 +30,19 @@ FileHelper.WriteToFile(list, "result.csv");
 //EssayHelpers.ConvertAndSaveEssays("C:\\AuthentiWrite\\docs\\example_AI_3.txt", "C:\\AuthentiWrite\\datasets\\example_3.txt", "AI");
 //EssayHelpers.ConvertAndSaveEssays("C:\\AuthentiWrite\\docs\\example_H_3.txt", "C:\\AuthentiWrite\\datasets\\example_3.txt", "H");
 
-//// Step 2 - Load the list of essays from disk.
-//var myEssays = EssayHelpers.LoadEssaysFromJson("C:\\AuthentiWrite\\datasets\\example_3.txt");
+// Step 2 - Load the list of essays from disk.
+var myEssays = EssayHelpers.LoadEssaysFromJson("C:\\AuthentiWrite\\datasets\\example_3.txt");
 
-//// Step 3 - Perform Feature Engineering / Feature Extraction
-//List<Text> texts = myEssays.Select(x => new Text(x.Content, isAIGenerated: x.Author == "AI")).ToList();
+// Step 3 - Perform Feature Engineering / Feature Extraction
+List<Text> texts = myEssays.Select(x => new Text(x.Content, isAIGenerated: x.Author == "AI")).ToList();
 
-//// Step 4 - Convert results to the dataset format.
-//MLDataset dataset = texts.ConvertToMLDataset(nameof(Text.IsAIGenerated));
+// Step 4 - Convert results to the dataset format.
+MLDataset dataset = texts.ConvertToMLDataset(nameof(Text.IsAIGenerated));
 
-//// Step 5 - Verify the dataset
-//dataset.Print();
+// Step 5 - Verify the dataset
+dataset.Print();
 
-//// Step 6 - Save the dataset to disk
-//dataset.SaveAsJson("C:\\AuthentiWrite\\datasets\\example_3_dataset.json");
+// Step 6 - Save the dataset to disk
+dataset.SaveAsJson("C:\\AuthentiWrite\\datasets\\example_3b_dataset.json");
 
-//Console.WriteLine("Done!");
+Console.WriteLine("Done!");
